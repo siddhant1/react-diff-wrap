1cfbeff713047ddfdf6ddc9316c3e2c86d77d6c3
siddhant1 <sidhant.manchanda@gmail.com>
2020-01-27 15:09:59 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import HrDiffWrap from "./HrDiffWrap";
+import HrDiffWrap from "./Differ";
 
 function App() {
   return (
